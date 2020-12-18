2fea70286bfa6d018d57f982c6f6d27bb997c7b7
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2020-12-18 10:19:31 +0530

    remove vendor from versioning

--- vendor/bundle/ruby/2.3.0/gems/ffi-1.9.10/ext/ffi_c/libffi-x86_64-linux-gnu/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/home/apoorv/MEGA/github/apoorv98.github.io/vendor/bundle/ruby/2.3.0/gems/ffi-1.9.10/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/bundle/ruby/2.3.0/gems/github-pages-health-check-1.0.1/lib/github-pages-health-check/version.rb
+++
@@ -1,5 +0,0 @@
-module GitHubPages
-  module HealthCheck
-    VERSION = "1.0.1"
-  end
-end
